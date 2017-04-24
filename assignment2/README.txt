dc763cb96dad8307899cdadc9735eaff4400229c
leald <leald@carleton.edu>
2017-04-24 15:56:24 -0500

    Finalizes assignment 2

---
+++ assignment2/README.txt
@@ -0,0 +1,5 @@
+README.txt
+
+The dataset described by the following visualizations pertains to the challenger incident of 1986. It contains information about o-ring damage, temperatures on the day of the launch, as well as flight indices.
+To view the websites, simply open the html files (scatterplotMatrix.html or parallelCoordinates.html) in a browser (the associated javascript files must be in the same directory). This can be easily done by running a python3 server in the root directory and navigating to the desired files in the browser.
+Interactions with the visualizations are simple. In the scatterplot matrix it is possible to mouseover points to highlight them and display their values. Further, clicking on a point will highlight the data item in all of the plots. In the parallel coordinates site, data paths are also highlightable and clickable using the mouse.
--- assignment2/scatterplotMatrix.js
+++ assignment2/scatterplotMatrix.js
@@ -1,18 +1,22 @@
-// Part of a brief D3 tutorial.
-// Upon completion, will display an interactive scatterplot showing relationship between
-//   different values associated with the top 100 words in Shakespeare's First Folio
-// CS 314, Spring 2017
-// Eric Alexander
+/*
+Danny Leal, 04/20/17
+scatterplotMatrix.js,
+This script creates a scatterplot matrix of data describing the
+Challenger incident of 1986.
 
-// First, we will create some constants to define non-data-related parts of the visualization
-w = 300;			// Width of our visualization
-h = 170;			// Height of our visualization
-xOffset = 40;		// Space for x-axis labels
-yOffset = 150;		// Space for y-axis labels
-margin = 10;		// Margin around visualization
+This code was written by adopting Eric Alexander's "d3_lab" code.
+*/
+
+//Sizing variables
+w = 310
+h = 164;
+xOffset = 20;
+yOffset = 30;
+margin = 10;
+//key values from the dataset
 vals = ['flight_index', 'num_o_ring_distress', 'launch_temp', 'leak_check_pressure', 'tufte_metric'];
 
-// Next, we will load in our CSV of data
+// Initialize a scatterplot for each span element
 initScatterplot('#pointsSVG1', vals[1], vals[2]);
 initScatterplot('#pointsSVG2', vals[1], vals[3]);
 initScatterplot('#pointsSVG3', vals[1], vals[4]);
@@ -26,11 +30,10 @@ initScatterplot('#pointsSVG10', vals[4], vals[1]);
 initScatterplot('#pointsSVG11', vals[4], vals[2]);
 initScatterplot('#pointsSVG12', vals[4], vals[3]);
 
-
 function initScatterplot(svgElement, xVal, yVal) {
+	//Import data
 	d3.csv('challenger.csv', function(csvData) {
-		// This will define scales that convert values
-		// from our data domain into screen coordinates.
+		// Define scales for converting values to screen coordinates
 		xScale = d3.scale.linear()
 					.domain([d3.min(csvData, function(d) { return parseFloat(d[xVal]); })-1,
 							 d3.max(csvData, function(d) { return parseFloat(d[xVal]); })+1])
@@ -40,12 +43,12 @@ function initScatterplot(svgElement, xVal, yVal) {
 							 d3.max(csvData, function(d) { return parseFloat(d[yVal]); })+1])
 					.range([h - xOffset - margin, margin]); // Notice this is backwards!
 
-		// Next, we will create an SVG element to contain our visualization.
+		// Create svgs to contain scatterplots
 		svg = d3.select(svgElement).append('svg:svg')
 					.attr('width', w)
 					.attr('height', h);
 
-		// Build axes! (These are kind of annoying, actually...)
+		// Build axes
 		xAxis = d3.svg.axis()
 					.scale(xScale)
 					.orient('bottom')
@@ -54,12 +57,6 @@ function initScatterplot(svgElement, xVal, yVal) {
 					.attr('class', 'axis')
 					.attr('transform', 'translate(0,' + (h - xOffset) + ')')
 					.call(xAxis);
-		xLabel = svg.append('text')
-					.attr('class','label')
-					.attr('x', w/2)
-					.attr('y', h - 5)
-					.text(xVal);
-
 		yAxis = d3.svg.axis()
 					.scale(yScale)
 					.orient('left')
@@ -68,67 +65,55 @@ function initScatterplot(svgElement, xVal, yVal) {
 					.attr('class', 'axis')
 					.attr('transform', 'translate(' + yOffset + ',0)')
 					.call(yAxis);
-		yLabel = svg.append('text')
-					.attr('class','label')
-					.attr('x', yOffset/2)
-					.attr('y', h/2-10)
-					.text(yVal);
 
-
-		// Now, we will start actually building our scatterplot!
-		// *****************************************************
-		// ************** YOUR CODE WILL GO HERE! **************
-		// *****************************************************
-
+		// Setup tooltip
 		var tooltip = d3.select("body").append("div")
 		    .attr("class", "tooltip")
 		    .style("opacity", 0);
-			// Select elements
 
-		// Bind data to elements
+		// Bind data to circles
 		var circle = svg.selectAll('circle')
         	.data(csvData);
 
-		// Create new elements if needed
+		// Create circles
 		circle.enter()
 			.append('svg:circle');
 
-		// Update our selection
-		// Give it a class
-		circle.attr('class', 'circle')
+		// Update circles so that each is associated with a different data item
+		circle.attr('class', function(d) { return 'flight_' + d['flight_index']; })
 			// x-coordinate
 			.attr('cx', function(d) { return xScale(d[xVal]); })
 			// y-coordinate
 			.attr('cy', function(d) { return yScale(d[yVal]); })
 			// radius
-			.attr('r', 3.5)
+			.attr('r', 4.0)
             // color
 			.style('fill', 'steelblue')
-			// tooltip
+			// Setup interactive functions
 			.on("mouseover", function(d) {
-				if (d3.select(this).attr('r') != 4.7) {
-					d3.select(this).attr('r', 4.5).style("fill", "orange");
-		            tooltip.transition()
-		                .duration(200)
-		                .style("opacity", .9);
-					tooltip.text("X: " + d[xVal] + "\n" + "Y: " + d[yVal])
-						.style("top", (d3.event.pageY + 16) + "px")
-						.style("left", (d3.event.pageX + 16) + "px");
+				if (d3.select(this).attr('r') != 6.0) {
+					d3.select(this).attr('r', 5.5).style("fill", "red");
 				}
+	            tooltip.transition()
+	                .duration(200)
+	                .style("opacity", .9);
+				tooltip.text(xVal + ": " + d[xVal] + "\n" + yVal + ": " + d[yVal])
+					.style("top", (d3.event.pageY + 16) + "px")
+					.style("left", (d3.event.pageX + 16) + "px");
 	            })
 	        .on("mouseout", function(d) {
-				if (d3.select(this).attr('r') != 4.7) {
-					d3.select(this).attr('r', 3.5).style("fill", "steelblue");
+				if (d3.select(this).attr('r') != 6.0) {
+					d3.select(this).attr('r', 4.0).style("fill", "steelblue");
 		            tooltip.transition()
 		                .duration(500)
 		                .style("opacity", 0);
 				}
 	        })
 			.on("click", function(d) {
-				if (d3.select(this).attr('r') == 3.5 || d3.select(this).attr('r') == 4.5) {
-					d3.select(this).attr('r', 4.7).style("fill", "orange");
+				if (d3.select(this).attr('r') == 4.0 || d3.select(this).attr('r') == 5.5) {
+					d3.selectAll('.flight_' + d['flight_index']).attr('r', 6.0).style("fill", "orange");
 				} else {
-					d3.select(this).attr('r', 3.5).style("fill", "steelblue");
+					d3.selectAll('.flight_' + d['flight_index']).attr('r', 4.0).style("fill", "steelblue");
 				}
 			});
 	});
